cc9a60baa80dac271c340ba05a3d583db1b7324c
Krisztian Holub <kholub1989@gmail.com>
2020-04-13 19:53:23 -0400

    parsing form data

--- index.js
+++ index.js
@@ -2,7 +2,29 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hi there');
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/', (req, res) => {
+  req.on('data', (data) => {
+    const parsed = data.toString('utf8').split('&');
+    const formData = {};
+    for (let pair of parsed) {
+      const [key, value] = pair.split('=');
+      formData[key] = value;
+    }
+    console.log(formData);
+  });
+  res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
