7b7f03a7fbac7224595f0d9290429375ce000265
Krisztian Holub <kholub1989@gmail.com>
2020-04-13 19:10:29 -0400

    creating a web server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi there');
+});
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
