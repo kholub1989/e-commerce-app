b5c4f4f00858af1ee5cfba8ccee01100bf13c634
Krisztian Holub <kholub1989@gmail.com>
2020-04-14 12:58:43 -0400

    updating record

--- repositories/users.js
+++ repositories/users.js
@@ -50,12 +50,23 @@ class UsersRepository {
     const filteredRecords = records.filter((record) => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.delete('adbeec41');
+  await repo.update('dsfsdf34fsd', { password: 'mypass' });
 };
 
 test();
