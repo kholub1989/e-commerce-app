ad5614dfd851872a43a8e2da0a15f88a89338cc8
Krisztian Holub <kholub1989@gmail.com>
2020-04-14 11:31:57 -0400

    opening the repo data file

--- repositories/users.js
+++ repositories/users.js
@@ -12,6 +12,21 @@ class UsersRepository {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+  async getAll() {
+    // Open the file called this.filename
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: 'utf-8',
+    });
+    // Read its content
+    console.log(contents);
+    // Parse the content
+    // Return the parsed data
+  }
 }
 
-const repo = new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+  await repo.getAll();
+};
+
+test();
