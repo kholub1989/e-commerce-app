47c0aaba8d661c42ec41223ac1f44f949ca1e116
Krisztian Holub <kholub1989@gmail.com>
2020-04-14 13:24:15 -0400

    exporting an instance

--- repositories/users.js
+++ repositories/users.js
@@ -78,13 +78,4 @@ class UsersRepository {
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository('users.json');
-
-  const user = await repo.getOneBy({
-    id: '6588b416',
-  });
-  console.log(user);
-};
-
-test();
+module.exports = new UsersRepository('users.json');
