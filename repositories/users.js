26a5b7b4b6838c301dfd231bb495aa731ab74ea0
Krisztian Holub <kholub1989@gmail.com>
2020-04-14 12:09:58 -0400

    saving records

--- repositories/users.js
+++ repositories/users.js
@@ -19,11 +19,21 @@ class UsersRepository {
       })
     );
   }
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
+
+  await repo.create({ email: 'test@test.com', password: 'password' });
+
   const users = await repo.getAll();
+
   console.log(users);
 };
 
