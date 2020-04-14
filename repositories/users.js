b50ac973251d3e3edaef1c55a8292849e28b9bf3
Krisztian Holub <kholub1989@gmail.com>
2020-04-14 11:35:34 -0400

    small refactor

--- repositories/users.js
+++ repositories/users.js
@@ -13,20 +13,18 @@ class UsersRepository {
     }
   }
   async getAll() {
-    // Open the file called this.filename
-    const contents = await fs.promises.readFile(this.filename, {
-      encoding: 'utf-8',
-    });
-    // Read its content
-    console.log(contents);
-    // Parse the content
-    // Return the parsed data
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf-8',
+      })
+    );
   }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
-  await repo.getAll();
+  const users = await repo.getAll();
+  console.log(users);
 };
 
 test();
