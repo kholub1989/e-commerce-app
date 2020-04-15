d8bcbbcb60628e0c5f8d619bf3794a6296a7facf
Krisztian Holub <kholub1989@gmail.com>
2020-04-15 10:37:23 -0400

    signing in user

--- index.js
+++ index.js
@@ -59,7 +59,21 @@ app.get('/signin', (req, res) => {
   `);
 });
 
-app.post('/signin', (req, res) => {});
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not found');
+  }
+
+  if (user.password !== password) {
+    return res.send('Invalid password');
+  }
+
+  req.session.userId = user.id;
+  res.send('You are signed in');
+});
 
 app.listen(3000, () => {
   console.log('Listening');
