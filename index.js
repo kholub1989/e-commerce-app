165dd933acb28098446ea6d1debc7d027635aeff
Krisztian Holub <kholub1989@gmail.com>
2020-04-15 10:30:32 -0400

    signing out user

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
       Your ID is: ${req.session.userId}
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
   const existingUser = await usersRepo.getOneBy({ email });
   if (existingUser) {
@@ -41,6 +41,25 @@ app.post('/', async (req, res) => {
 
   res.send('Account created!!!');
 });
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/signin', (req, res) => {});
 
 app.listen(3000, () => {
   console.log('Listening');
