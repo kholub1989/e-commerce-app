424b17fe254ced8f5fe3ceee399fcc9a7d9fefe4
Krisztian Holub <kholub1989@gmail.com>
2020-04-15 09:01:46 -0400

    creatin users record

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.post('/', async (req, res) => {
   if (password !== passwordConfirmation) {
     return res.send('Password must match');
   }
+
+  // Create a user in our user repo to represent this person
+  const user = await usersRepo.create({ email, password });
+  // Store the id of that user inside the user cookie
+
   res.send('Account created!!!');
 });
 
--- repositories/users.js
+++ repositories/users.js
@@ -27,6 +27,7 @@ class UsersRepository {
     const records = await this.getAll();
     records.push(attrs);
     await this.writeAll(records);
+    return attrs;
   }
 
   async writeAll(records) {
