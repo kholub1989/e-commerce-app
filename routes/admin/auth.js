6d3582f10fef0e8fa7776cd3c687af97d48773e5
Krisztian Holub <kholub1989@gmail.com>
2020-04-21 10:13:19 -0400

    extracting validation chains

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -3,6 +3,11 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+} = require('./validators');
 
 const router = express.Router();
 
@@ -12,32 +17,7 @@ router.get('/signup', (req, res) => {
 
 router.post(
   '/signup',
-  [
-    check('email')
-      .trim()
-      .normalizeEmail()
-      .isEmail()
-      .withMessage('Must be a valid email')
-      .custom(async (email) => {
-        const existingUser = await usersRepo.getOneBy({ email });
-        if (existingUser) {
-          throw new Error('Email in use');
-        }
-      }),
-    check('password')
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage('Must be between 4 and 20 characters'),
-    check('passwordConfirmation')
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage('Must be between 4 and 20 characters')
-      .custom((passwordConfirmation, { req }) => {
-        if (passwordConfirmation !== req.body.password) {
-          throw new Error('Passwords must match');
-        }
-      }),
-  ],
+  [requireEmail, requirePassword, requirePasswordConfirmation],
   async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
