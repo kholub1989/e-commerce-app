b8b0ba0fd9c5a81de2f09f3a05af3352ba5cd789
Krisztian Holub <kholub1989@gmail.com>
2020-04-21 09:59:20 -0400

    adding costum validators

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -13,21 +13,35 @@ router.get('/signup', (req, res) => {
 router.post(
   '/signup',
   [
-    check('email').trim().normalizeEmail().isEmail(),
-    check('password').trim().isLength({ min: 4, max: 20 }),
-    check('passwordConfirmation').trim().isLength({ min: 4, max: 20 }),
+    check('email')
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Must be a valid email')
+      .custom(async (email) => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) {
+          throw new Error('Email in use');
+        }
+      }),
+    check('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Must be between 4 and 20 characters'),
+    check('passwordConfirmation')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Must be between 4 and 20 characters')
+      .custom((passwordConfirmation, { req }) => {
+        if (passwordConfirmation !== req.body.password) {
+          throw new Error('Passwords must match');
+        }
+      }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
     const { email, password, passwordConfirmation } = req.body;
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-      return res.send('Email in use');
-    }
-    if (password !== passwordConfirmation) {
-      return res.send('Password must match');
-    }
 
     // Create a user in our user repo to represent this person
     const user = await usersRepo.create({ email, password });
