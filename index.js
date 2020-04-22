efceb29cc6188bf80e82185988056e5cba878fe1
Krisztian Holub <kholub1989@gmail.com>
2020-04-22 13:46:36 -0400

    adding products routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 app.use(express.static('public'));
@@ -12,6 +13,7 @@ app.use(
   })
 );
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
