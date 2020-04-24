f5ce8c2237e006f4137f4e3646f905e2e53f93d8
Krisztian Holub <kholub1989@gmail.com>
2020-04-24 09:35:18 -0400

    subtle middleware bug

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -17,11 +17,14 @@ router.get('/admin/products/new', (req, res) => {
 
 router.post(
   '/admin/products/new',
-  [requireTitle, requirePrice],
   upload.single('image'),
+  [requireTitle, requirePrice],
   async (req, res) => {
     const errors = validationResult(req);
 
+    if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ errors }));
+    }
     const image = req.file.buffer.toString('base64');
     const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
