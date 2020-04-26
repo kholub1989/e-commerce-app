40ddd3e4dad921d34c7dd51f503a687b66fc7a5d
Krisztian Holub <kholub1989@gmail.com>
2020-04-26 10:41:34 -0400

    reqiring authentications

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -12,4 +12,11 @@ module.exports = {
       next();
     };
   },
+  requireAuth(req, res, next) {
+    if (!req.session.userId) {
+      return res.redirect('/signin');
+    }
+
+    next();
+  },
 };
