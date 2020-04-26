83b0819890a200294ed8c83e31f9c0f043288c90
Krisztian Holub <kholub1989@gmail.com>
2020-04-26 11:24:18 -0400

    displaying an edit form

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,7 +5,7 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
-// const productsEditTemplate = require('../../views/admin/products/edit');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
@@ -35,7 +35,7 @@ router.post(
   }
 );
 
-router.get('/admin/products/:id/edit', async (req, res) => {
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
   const product = await productsRepo.getOne(req.params.id);
   if (!product) {
     return res.send('Product nott found');
@@ -43,5 +43,7 @@ router.get('/admin/products/:id/edit', async (req, res) => {
 
   res.send(productsEditTemplate({ product }));
 });
+
+router.post('/admin/products/:id/edit', requireAuth, async (req, res) => {});
 
 module.exports = router;
