912f4df07863f5e76c1ee3d7781a2f95ba215e16
Krisztian Holub <kholub1989@gmail.com>
2020-04-27 10:27:05 -0400

    deleting Products functionality

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -68,4 +68,10 @@ router.post(
   }
 );
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+  await productsRepo.delete(req.params.id);
+
+  res.redirect('/admin/products');
+});
+
 module.exports = router;
