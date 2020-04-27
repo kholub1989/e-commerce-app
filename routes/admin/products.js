dea61def165589991d163a9d28ad2836974df88c
Krisztian Holub <kholub1989@gmail.com>
2020-04-27 10:00:44 -0400

    fixing the hendleerrors middleware

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -49,7 +49,10 @@ router.post(
   requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
-  handleErrors(productsEditTemplate),
+  handleErrors(productsEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
   async (req, res) => {
     const changes = req.body;
     if (req.file) {
