125b03e1126a83aef7efdb7ff9639c00fb49c6c1
Krisztian Holub <kholub1989@gmail.com>
2020-04-30 11:45:00 -0400

    shopping cart boilerplate

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products');
 const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
 const app = express();
 app.use(express.static('public'));
@@ -16,6 +17,7 @@ app.use(
 app.use(authRouter);
 app.use(productsRouter);
 app.use(adminProductsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Receive a POST req to add an item to a cart
+
+// recive a GET req to show all items in cart
+
+// Recive a POST req to del an item from a cart
+
+module.exports = router;
