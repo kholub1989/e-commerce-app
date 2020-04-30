e02e5375b1f70324e5949616757a25e6d7d78ecc
Krisztian Holub <kholub1989@gmail.com>
2020-04-30 11:52:04 -0400

    submission oprion

--- routes/carts.js
+++ routes/carts.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 // Receive a POST req to add an item to a cart
-
+router.post('/cart/products', (req, res) => {
+  console.log(req.body.productId);
+  res.send('Product added to cart');
+});
 // recive a GET req to show all items in cart
 
 // Recive a POST req to del an item from a cart
