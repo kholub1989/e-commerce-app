50722ec41eeb5d2864e7b18e798b583dfc2a3d51
Krisztian Holub <kholub1989@gmail.com>
2020-05-05 08:32:22 -0400

    redirect on remove

--- routes/carts.js
+++ routes/carts.js
@@ -33,7 +33,7 @@ router.post('/cart/products', async (req, res) => {
     items: cart.items,
   });
 
-  res.send('Product added to cart');
+  res.redirect('/cart');
 });
 // recive a GET req to show all items in cart
 router.get('/cart', async (req, res) => {
@@ -52,6 +52,13 @@ router.get('/cart', async (req, res) => {
 
 // Recive a POST req to del an item from a cart
 router.post('/cart/products/delete', async (req, res) => {
-  console.log(req.body.itemId);
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+
+  await cartsRepo.update(req.session.cartId, { items });
+
+  res.redirect('/cart');
 });
 module.exports = router;
